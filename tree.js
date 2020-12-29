8d09877e028c1ee4aac1d528c51ac8c39e3ab025
XiaoPan <2489964425@qq.com>
2020-12-29 12:00:20 +0800

    新增节点

--- tree.js
+++ tree.js
@@ -6,3 +6,17 @@ const createTree = value => {
     parent:null
   }
 }
+
+//新增节点
+const addChild = (node,value) => {
+  // 新生一个节点，在node上新增一个newNode节点
+  const newNode = {
+    data:value,
+    children:null,
+    parent:node
+  }
+  // node有无子结点，有就push，没有就新增再push
+  node.children = node.children || []
+  node.children.push(newNode)
+  return newNode
+}
